12489fdfad478aacbc9efc727cbc163b5659ad58
Rinat Sadykov <sadykovra@mail.ru>
2017-08-28 20:19:30 +0500

    Исправил задание #12

--- js/map.js
+++ js/map.js
@@ -1,4 +1,8 @@
 'use strict';
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var PIN_WIDTH = 40;
+var PIN_HEIGHT = 40;
 var generatorOptions = {
   FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
   TITLES: ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
@@ -59,8 +63,8 @@ function generateUniqueURL(minAvatarID, maxAvatarID, generatedOffer) {
   return randomURL;
 }
 
-function getHumanFriendlyType(machine) {
-  switch (machine) {
+function getHumanFriendlyType(type) {
+  switch (type) {
     case 'flat':
       var humanType = generatorOptions.TYPE_DESCS[0];
       break;
@@ -98,27 +102,35 @@ function getRandomFeatures(options) {
   return result;
 }
 
-function renderPin(generatedOffer) {
-  var adsElement = nearbyAdsItem.cloneNode(true);
+function renderPin(generatedOffer, idx) {
+  var adsElement = document.createElement('div');
+  var adsImg = document.createElement('img');
+  adsElement.appendChild(adsImg);
+  adsElement.classList.add('pin');
   adsElement.style.left = generatedOffer.location.x + 'px';
   adsElement.style.top = generatedOffer.location.y + 'px';
-  adsElement.querySelector('img').src = generatedOffer.author.avatar;
+  adsElement.tabIndex = 0;
+  adsImg.src = generatedOffer.author.avatar;
+  adsImg.classList.add('rounded');
+  adsImg.style.width = PIN_WIDTH + 'px';
+  adsImg.style.height = PIN_HEIGHT + 'px';
+  adsElement.dataset.index = idx;
   return adsElement;
 }
 
 var nearbyAdsList = document.querySelector('.tokyo__pin-map');
-var nearbyAdsItem = document.querySelector('.pin');
 var fragment = document.createDocumentFragment();
 var offerDialog = document.querySelector('.dialog');
-var dialogDesc = document.querySelector('.dialog__panel');
-var dialogTemplate = document.querySelector('#lodge-template').content;
+var dialogTemplateCopy = document.querySelector('#lodge-template').content;
 
-for (i = 0; i < randomOffers.length; i++) {
-  fragment.appendChild(renderPin(randomOffers[i]));
-}
+randomOffers.forEach(function (elem, idx) {
+  fragment.appendChild(renderPin(elem, idx));
+});
 nearbyAdsList.appendChild(fragment);
 
-function renderOffers(generatedOffer) {
+function renderOffer(generatedOffer) {
+  var dialogDesc = document.querySelector('.dialog__panel');
+  var dialogTemplate = dialogTemplateCopy.cloneNode(true);
   offerDialog.replaceChild(dialogTemplate, dialogDesc);
   offerDialog.querySelector('.lodge__title').textContent = generatedOffer.offer.title;
   offerDialog.querySelector('.lodge__address').textContent = generatedOffer.offer.address;
@@ -131,8 +143,194 @@ function renderOffers(generatedOffer) {
   });
   offerDialog.querySelector('.lodge__description').textContent = generatedOffer.offer.description;
   offerDialog.querySelector('.dialog__title').querySelector('img').src = generatedOffer.author.avatar;
+
 }
 
-for (var i = 0; i < 1; i++) {
-  renderOffers(randomOffers[i]);
+// Задание 11//
+
+var pins = nearbyAdsList.querySelectorAll('.pin');
+var closeDialogBtn = offerDialog.querySelector('.dialog__close');
+
+function deactivateLastPin() {
+  pins.forEach(function (elem) {
+    if (elem.classList.contains('pin--active')) {
+      elem.classList.remove('pin--active');
+    }
+  });
 }
+
+var onDialogEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeDialog();
+  }
+};
+
+var openDialog = function (elem) { // функция открытия popup
+  offerDialog.classList.remove('hidden');
+  deactivateLastPin();
+  elem.classList.add('pin--active');
+  renderOffer(randomOffers[elem.dataset.index]);
+  document.addEventListener('keydown', onDialogEscPress);
+};
+
+var closeDialog = function () { // функция закрытия popup
+  deactivateLastPin();
+  offerDialog.classList.add('hidden');
+  document.removeEventListener('keydown', onDialogEscPress);
+};
+
+
+pins.forEach(function (elem) { // вызываем функцию на каждом элементе массива pins при нажатии
+  if (!elem.classList.contains('pin__main')) {
+    elem.addEventListener('click', function () {
+      openDialog(elem);
+    });
+  }
+});
+
+closeDialogBtn.addEventListener('click', closeDialog);
+
+pins.forEach(function (elem) { // открываем popup при помощи enter
+  elem.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      openDialog(elem);
+    }
+  });
+});
+
+
+closeDialogBtn.addEventListener('keydown', function (evt) { // закрываем popup при помощи enter
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeDialog();
+  }
+});
+
+
+// Задание 12
+var form = document.querySelector('.notice__form');
+var titleField = form.querySelector('.form__title');
+var timeInField = form.querySelector('.form__timein');
+var timeOutField = form.querySelector('.form__timeout');
+var roomNumbersField = form.querySelector('.form__room_number');
+var addressField = form.querySelector('.form__address');
+var typeField = form.querySelector('.form__type');
+var priceField = form.querySelector('.form__price');
+var capacityField = form.querySelector('.form__capacity');
+
+function setDefaultFieldColor(field) {
+  field.style.border = '1px solid rgb(217, 217, 211)';
+}
+
+function setInvalidFieldColor(field) {
+  field.style.borderColor = 'rgb(255, 100, 100)';
+}
+
+function validationAddress(evt) {
+  if (addressField.value === '') {
+    setInvalidFieldColor(addressField);
+    evt.preventDefault();
+    setTimeout(setDefaultFieldColor, 1500, addressField);
+  }
+}
+
+function validationTitle(evt) {
+  if (titleField.value.length < 30 || titleField.value.length > 100) {
+    setInvalidFieldColor(titleField);
+    evt.preventDefault();
+    setTimeout(setDefaultFieldColor, 1500, titleField);
+  }
+}
+
+function validationPrice(minPrice) {
+  if (priceField.value < minPrice) {
+    setInvalidFieldColor(priceField);
+    setTimeout(setDefaultFieldColor, 1500, priceField);
+  } else {
+    return true;
+  }
+}
+
+form.addEventListener('submit', validationTitle);
+form.addEventListener('submit', validationAddress);
+
+timeInField.addEventListener('change', function () {
+  timeOutField.selectedIndex = timeInField.selectedIndex;
+});
+
+timeOutField.addEventListener('change', function () {
+  timeInField.selectedIndex = timeOutField.selectedIndex;
+});
+
+function syncTypeWithPrice(evt) {
+  switch (typeField.selectedIndex) {
+    case 0:
+      priceField.setAttribute('min', 1000); // эти 4 строки не получается засунуть в функцию validationPrice, потому что параметр evt почему не передается через две функции
+      var result = validationPrice(1000);
+      if (result !== true) {
+        evt.preventDefault();
+      }
+      break;
+    case 1:
+      priceField.setAttribute('min', 0);
+      result = validationPrice(0);
+      if (result !== true) {
+        evt.preventDefault();
+      }
+      break;
+    case 2:
+      priceField.setAttribute('min', 5000);
+      result = validationPrice(5000);
+      if (result !== true) {
+        evt.preventDefault();
+      }
+      validationPrice(5000);
+      break;
+    case 3:
+      priceField.setAttribute('min', 10000);
+      result = validationPrice(10000);
+      if (result !== true) {
+        evt.preventDefault();
+      }
+      validationPrice(10000);
+      break;
+  }
+}
+
+form.addEventListener('submit', syncTypeWithPrice);
+
+function syncRoomWithCapacity() {
+  switch (roomNumbersField.selectedIndex) {
+    case 0:
+      capacityField.selectedIndex = 2;
+      break;
+    case 1:
+      capacityField.selectedIndex = 1;
+      break;
+    case 2:
+      capacityField.selectedIndex = 0;
+      break;
+    case 3:
+      capacityField.selectedIndex = 3;
+      break;
+  }
+}
+
+function syncCapacityWithRoom() {
+  switch (capacityField.selectedIndex) {
+    case 0:
+      roomNumbersField.selectedIndex = 2;
+      break;
+    case 1:
+      roomNumbersField.selectedIndex = 1;
+      break;
+    case 2:
+      roomNumbersField.selectedIndex = 0;
+      break;
+    case 3:
+      roomNumbersField.selectedIndex = 3;
+      break;
+  }
+}
+
+roomNumbersField.addEventListener('click', syncRoomWithCapacity);
+capacityField.addEventListener('click', syncCapacityWithRoom);
