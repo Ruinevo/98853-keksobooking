86ad8d275f715084195ef8e48f18780ee6d3f4c6
Rinat Sadykov <sadykovra@mail.ru>
2017-09-05 23:53:19 +0500

    Выполнил задание #18

--- js/map.js
+++ js/map.js
@@ -8,28 +8,37 @@ window.map = (function (data, card, pin) {
   // 2. C помощью модуля pin отрисовывает данные на карте
   var nearbyAdsList = document.querySelector('.tokyo__pin-map');
 
+  function deactivateLastPin() {
+    var pins = document.querySelectorAll('.pin');
+    pins.forEach(function (elem) {
+      if (elem.classList.contains('pin--active')) {
+        elem.classList.remove('pin--active');
+      }
+    });
+  }
+
   function onPinClick(evt) {
-    card.openDialog(randomOffers[evt.currentTarget.dataset.index]);
+    card.showCard(randomOffers[evt.currentTarget.dataset.index]);
+    deactivateLastPin();
     evt.currentTarget.classList.add('pin--active');
   }
 
 
   function onPinEnterPress(evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
-      card.openDialog(randomOffers[evt.currentTarget.dataset.index]);
+      card.showCard(randomOffers[evt.currentTarget.dataset.index]);
+      deactivateLastPin();
       evt.currentTarget.classList.add('pin--active');
     }
   }
 
+
+  // отрисовываем пины на карте
   var fragment = document.createDocumentFragment();
   randomOffers.forEach(function (elem, idx) {
     fragment.appendChild(pin.renderPin(elem, idx));
   });
   nearbyAdsList.appendChild(fragment);
-
-  // 3. Отрисовывает данные в диалоге с помощью модуля card.js
-
-  card.renderOffer(randomOffers[1]);
 
   var pins = nearbyAdsList.querySelectorAll('.pin');
   pins.forEach(function (elem) {
@@ -105,6 +114,10 @@ window.map = (function (data, card, pin) {
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   });
+
+  return {
+    deactivateLastPin: deactivateLastPin
+  };
 
 })(window.data, window.card, window.pin);
 
